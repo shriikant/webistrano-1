84b483d50afabc57cb626784c19d323773bd11bd
辻 隆太郎 <kametaro-main@tsuji-no-Mac-mini.local>
2013-01-03 23:05:08 +0900

    process decrease

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,21 +1,11 @@
 # What the timeout for killing busy workers is, in seconds
-timeout 60
+timeout 30
  
 # Whether the app should be pre-loaded
 preload_app true
  
 # How many worker processes
-worker_processes 4
+worker_processes 2
 
 stdout_path "tmp/unicorn-out.log"
 stderr_path "tmp/unicorn-err.log"
- 
-# before/after forks
-before_fork do |server, worker|
-  sleep 1
-end
-
-after_fork do |server, worker|
-  GC.disable
-end
-

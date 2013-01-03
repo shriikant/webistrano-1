6da73385346e5393b656dcb1179fbd5429043022
辻 隆太郎 <kametaro-main@tsuji-no-Mac-mini.local>
2013-01-03 23:20:19 +0900

    log add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Webistrano::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
